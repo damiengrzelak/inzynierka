965be82b6049a9b62327d5188898647a0600af29
Damian Grzelak <dgrzelak@9bits.pl>
2018-01-17 12:37:29 +0100

    some fixes and improvement

---
+++ app/src/main/java/wfiis/pizzerialesna/customViews/LoaderView.java
@@ -0,0 +1,8 @@
+package wfiis.pizzerialesna.customViews;
+
+/**
+ * Created by DamianGrzelak on 17.01.2018.
+ */
+
+public class LoaderView {
+}
---
+++ app/src/main/java/wfiis/pizzerialesna/fragments/SplashFragment.java
@@ -0,0 +1,8 @@
+package wfiis.pizzerialesna.fragments;
+
+/**
+ * Created by DamianGrzelak on 17.01.2018.
+ */
+
+public class SplashFragment {
+}
